c4f34d263631d0b650e83b5934ff4aac34ea00d8
L D <tapircode@gmail.com>
2019-02-16 23:49:01 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,6 +3,24 @@ function incrementVariable() {
   return i;
 }
 
+function forLoop(array){
+  for (var i = 0; i < 25 ; i++){
+    if (i == 1){
+      array.push("I am 1 strange loop.")
+    } else {
+      array.push("I am ${i} strange loops.")
+    }
+  }
+}
+
+function whileLoop(n){
+  while( n >= 0 ){
+    console.log(n)
+    n = n - 1
+  }
+  console.log("done")
+}
+
 function doWhileLoop(n){
   do{
     console.log("I run once regardless.")
